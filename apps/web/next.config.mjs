e7e185477475a166215e20c3af88647c63685fdc
SebastianPellitero <sebastian.pellitero@gmail.com>
2026-05-17 19:45:52 +0200

    fix duplicate react for vercel

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@holiday-planner/shared'],
+  webpack(config) {
+    config.resolve.alias['react'] = path.resolve(__dirname, '../../node_modules/react')
+    config.resolve.alias['react-dom'] = path.resolve(__dirname, '../../node_modules/react-dom')
+    return config
+  },
 }
 
 export default nextConfig
